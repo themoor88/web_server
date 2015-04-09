828d2bcfe51dc6d2a1348fa771b3b6e5f494b614
Wajid Kagzi <wjkagzi@gmail.com>
2015-04-08 20:22:06 -0400

    Cleaned up and finally understand server.rb while adding CSS.

--- server.rb
+++ server.rb
@@ -15,9 +15,45 @@ loop do                                             # Server runs forever
   end
   puts lines                                        # Output the full request to stdout
 
-  filename = "index.html"
-  response = File.read(filename)
+  filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
 
-  client.puts(response)                       # Output the current time to the client
+  if File.exists?(filename)
+    response_body = File.read(filename)
+
+    # Added a case by case method to output type for HTML document.
+    case filename
+      when /\.css/
+        type = "text/css"
+      when /\.js/
+        type = "text/javascript"
+      when /\.png/
+        type = "image/png"
+      when /\.jpe?g/
+        type = "image/jpeg"
+      else
+        type = "text/html"
+    end
+
+    success_header = []
+    success_header << "HTTP/1.1 200 OK"
+    success_header << "Content-Type: #{type}"       # should reflect the appropriate content type (HTML, CSS, text, etc)
+    success_header << "Content-Length: #{response_body.length}" # should be the actual size of the response body
+    success_header << "Connection: close"
+    header = success_header.join("\r\n")
+
+  else
+    response_body = "File Not Found\n"              # need to indicate end of the string with \n
+
+    not_found_header = []
+    not_found_header << "HTTP/1.1 404 Not Found"
+    not_found_header << "Content-Type: text/plain"      # is always text/plain
+    not_found_header << "Content-Length: #{response_body.length}" # should the actual size of the response body
+    not_found_header << "Connection: close"
+    header = not_found_header.join("\r\n")
+  end
+
+  response = [header, response_body].join("\r\n\r\n")
+
+  client.puts(response)                             # Output the current time to the client
   client.close                                      # Disconnect from the client
 end
